900c3a34d17efe56a33348c8ad6f1b759f06f580
Chinmay Garg <64963908+nefario7@users.noreply.github.com>
2021-11-24 19:08:04 -0500

    EKF SLAM

--- Project 4/controllers/main/ekf_slam.py
+++ Project 4/controllers/main/ekf_slam.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-class EKF_SLAM():
+
+class EKF_SLAM:
     def __init__(self, init_mu, init_P, dt, W, V, n):
         """Initialize EKF SLAM
 
@@ -8,15 +9,14 @@ class EKF_SLAM():
         the location of map features
 
         Args:
-            init_mu: A numpy array of size (3+2*n, ). Initial guess of the mean 
-            of state. 
-            init_P: A numpy array of size (3+2*n, 3+2*n). Initial guess of 
+            init_mu: A numpy array of size (3+2*n, ). Initial guess of the mean
+            of state.
+            init_P: A numpy array of size (3+2*n, 3+2*n). Initial guess of
             the covariance of state.
             dt: A double. The time step.
             W: A numpy array of size (3+2*n, 3+2*n). Process noise
             V: A numpy array of size (2*n, 2*n). Observation noise
             n: A int. Number of map features
-            
 
         Returns:
             An EKF SLAM object.
@@ -24,7 +24,7 @@ class EKF_SLAM():
         self.mu = init_mu  # initial guess of state mean
         self.P = init_P  # initial guess of state covariance
         self.dt = dt  # time step
-        self.W = W  # process noise 
+        self.W = W  # process noise
         self.V = V  # observation noise
         self.n = n  # number of map features
 
@@ -41,6 +41,15 @@ class EKF_SLAM():
         Returns:
             x_next: A numpy array of size (3+2*n, ). The state at next time step
         """
+        xt = x[0]
+        yt = x[1]
+        psit = x[2]
+        m = x[3:]
+        x_next = np.zeros((3 + 2 * self.n,))
+        x_next[0] = xt + self.dt * (u[0] * np.cos(psit) - u[1] * np.sin(psit))
+        x_next[1] = yt + self.dt * (u[0] * np.sin(psit) + u[1] * np.cos(psit))
+        x_next[2] = self._wrap_to_pi(psit + self.dt * u[2])
+        x_next[3:] = x_next[3:] + x[3:]
 
         return x_next
 
@@ -56,6 +65,18 @@ class EKF_SLAM():
         Returns:
             y: A numpy array of size (2*n, ). The sensor measurement.
         """
+        xt = x[0]
+        yt = x[1]
+        psit = x[2]
+
+        m = x[3:].reshape(-1, 2).T
+        delta_mx = m[0, :] - xt
+        delta_my = m[1, :] - yt
+
+        h_distances = np.linalg.norm(np.vstack((delta_mx, delta_my)), ord=2, axis=0)
+        h_angles = self._wrap_to_pi(np.arctan2(delta_my, delta_mx) - psit)
+
+        y = np.hstack((h_distances, h_angles))
 
         return y
 
@@ -70,6 +91,13 @@ class EKF_SLAM():
         Returns:
             F: A numpy array of size (3+2*n, 3+2*n). The jacobian of f evaluated at x_k.
         """
+        x_dot = u[0]
+        y_dot = u[1]
+        psi = x[2]
+
+        F = np.eye(3 + 2 * self.n)
+        F[0, 2] = self.dt * (-x_dot * np.sin(psi) - y_dot * np.cos(psi))
+        F[1, 2] = self.dt * (x_dot * np.cos(psi) - y_dot * np.sin(psi))
 
         return F
 
@@ -83,9 +111,50 @@ class EKF_SLAM():
         Returns:
             H: A numpy array of size (2*n, 3+2*n). The jacobian of h evaluated at x_k.
         """
+        p = np.array((x[0], x[1]))
+        xt = x[0]
+        yt = x[1]
+        psit = x[2]
+
+        m = x[3:].reshape(-1, 2).T
+        delta_mx = m[0, :] - xt
+        delta_my = m[1, :] - yt
+        delta_m = np.vstack((delta_mx, delta_my))
+
+        norm = np.linalg.norm(delta_m, axis=0)
+        deld_xt = -delta_mx / norm
+        deld_yt = -delta_my / norm
+        deld_psit = np.zeros_like(deld_xt)
+        deld_mx = -deld_xt
+        deld_my = -deld_yt
+        deld_mxy = np.zeros((self.n, 2 * self.n))
+        for i in range(self.n):
+            deld_mxy[i, 2 * i] = deld_mx[i]
+            deld_mxy[i, 2 * i + 1] = deld_my[i]
+
+        deld_xt = deld_xt.reshape(-1, 1)
+        deld_yt = deld_yt.reshape(-1, 1)
+        deld_psit = deld_psit.reshape(-1, 1)
+
+        delr_xt = np.power(1 + np.power(delta_my / delta_mx, 2), -1) * (delta_my / np.power(delta_mx, 2))
+        delr_yt = -np.power(1 + np.power(delta_my / delta_mx, 2), -1) * (1 / delta_mx)
+        delr_psit = np.ones_like(delr_xt) * (-1)
+        delr_mx = -delr_xt
+        delr_my = -delr_yt
+        delr_mxy = np.zeros((self.n, 2 * self.n))
+        for i in range(self.n):
+            delr_mxy[i, i] = delr_mx[i]
+            delr_mxy[i, i + 1] = delr_my[i]
+
+        delr_xt = delr_xt.reshape(-1, 1)
+        delr_yt = delr_yt.reshape(-1, 1)
+        delr_psit = delr_psit.reshape(-1, 1)
+
+        h1 = np.concatenate((deld_xt, deld_yt, deld_psit, deld_mxy), axis=1)
+        h2 = np.concatenate((delr_xt, delr_yt, delr_psit, delr_mxy), axis=1)
+        H = np.concatenate((h1, h2), axis=0)
 
         return H
-
 
     def predict_and_correct(self, y, u):
         """Predice and correct step of EKF
@@ -102,14 +171,14 @@ class EKF_SLAM():
         # compute F
         F = self._compute_F(self.mu, u)
 
-        #***************** Predict step *****************#
+        # ***************** Predict step *****************#
         # predict the state
         self.mu = self._f(self.mu, u)
-        self.mu[2] =  self._wrap_to_pi(self.mu[2])
+        self.mu[2] = self._wrap_to_pi(self.mu[2])
         # predict the error covariance
         self.P = F @ self.P @ F.T + self.W
 
-        #***************** Correct step *****************#
+        # ***************** Correct step *****************#
         # compute H matrix
         H = self._compute_H(self.mu)
 
@@ -118,100 +187,90 @@ class EKF_SLAM():
 
         # update estimation with new measurement
         diff = y - self._h(self.mu)
-        diff[self.n:] = self._wrap_to_pi(diff[self.n:])
+        diff[self.n :] = self._wrap_to_pi(diff[self.n :])
         self.mu = self.mu + L @ diff
-        self.mu[2] =  self._wrap_to_pi(self.mu[2])
+        self.mu[2] = self._wrap_to_pi(self.mu[2])
 
         # update the error covariance
-        self.P = (np.eye(3+2*self.n) - L @ H) @ self.P
+        self.P = (np.eye(3 + 2 * self.n) - L @ H) @ self.P
 
         return self.mu, self.P
 
-
     def _wrap_to_pi(self, angle):
-        angle = angle - 2*np.pi*np.floor((angle+np.pi )/(2*np.pi))
+        angle = angle - 2 * np.pi * np.floor((angle + np.pi) / (2 * np.pi))
         return angle
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    m = np.array([[0.,  0.],
-                  [0.,  20.],
-                  [20., 0.],
-                  [20., 20.],
-                  [0,  -20],
-                  [-20, 0],
-                  [-20, -20],
-                  [-50, -50]]).reshape(-1)
+    m = np.array([[0.0, 0.0], [0.0, 20.0], [20.0, 0.0], [20.0, 20.0], [0, -20], [-20, 0], [-20, -20], [-50, -50]]).reshape(-1)
 
     dt = 0.01
     T = np.arange(0, 20, dt)
-    n = int(len(m)/2)
-    W = np.zeros((3+2*n, 3+2*n))
-    W[0:3, 0:3] = dt**2 * 1 * np.eye(3)
-    V = 0.1*np.eye(2*n)
-    V[n:,n:] = 0.01*np.eye(n)
+    n = int(len(m) / 2)
+    W = np.zeros((3 + 2 * n, 3 + 2 * n))
+    W[0:3, 0:3] = dt ** 2 * 1 * np.eye(3)
+    V = 0.1 * np.eye(2 * n)
+    V[n:, n:] = 0.01 * np.eye(n)
 
     # EKF estimation
-    mu_ekf = np.zeros((3+2*n, len(T)))
-    mu_ekf[0:3,0] = np.array([2.2, 1.8, 0.])
+    mu_ekf = np.zeros((3 + 2 * n, len(T)))
+    mu_ekf[0:3, 0] = np.array([2.2, 1.8, 0.0])
     # mu_ekf[3:,0] = m + 0.1
-    mu_ekf[3:,0] = m + np.random.multivariate_normal(np.zeros(2*n), 0.5*np.eye(2*n))
-    init_P = 1*np.eye(3+2*n)
+    mu_ekf[3:, 0] = m + np.random.multivariate_normal(np.zeros(2 * n), 0.5 * np.eye(2 * n))
+    init_P = 1 * np.eye(3 + 2 * n)
 
     # initialize EKF SLAM
-    slam = EKF_SLAM(mu_ekf[:,0], init_P, dt, W, V, n)
-    
-    # real state
-    mu = np.zeros((3+2*n, len(T)))
-    mu[0:3,0] = np.array([2, 2, 0.])
-    mu[3:,0] = m
+    slam = EKF_SLAM(mu_ekf[:, 0], init_P, dt, W, V, n)
 
-    y_hist = np.zeros((2*n, len(T)))
+    # real state
+    mu = np.zeros((3 + 2 * n, len(T)))
+    mu[0:3, 0] = np.array([2, 2, 0.0])
+    mu[3:, 0] = m
+
+    y_hist = np.zeros((2 * n, len(T)))
     for i, t in enumerate(T):
         if i > 0:
             # real dynamics
-            u = [-5, 2*np.sin(t*0.5), 1*np.sin(t*3)]
-            # u = [0.5, 0.5*np.sin(t*0.5), 0]
+            u = [-5, 2 * np.sin(t * 0.5), 1 * np.sin(t * 3)]
+            # u = [0.5, 0.5 * np.sin(t * 0.5), 0]
             # u = [0.5, 0.5, 0]
-            mu[:,i] = slam._f(mu[:,i-1], u) + \
-                np.random.multivariate_normal(np.zeros(3+2*n), W)
+            mu[:, i] = slam._f(mu[:, i - 1], u) + np.random.multivariate_normal(np.zeros(3 + 2 * n), W)
 
             # measurements
-            y = slam._h(mu[:,i]) + np.random.multivariate_normal(np.zeros(2*n), V)
-            y_hist[:,i] = (y-slam._h(slam.mu))
+            y = slam._h(mu[:, i]) + np.random.multivariate_normal(np.zeros(2 * n), V)
+            y_hist[:, i] = y - slam._h(slam.mu)
             # apply EKF SLAM
             mu_est, _ = slam.predict_and_correct(y, u)
-            mu_ekf[:,i] = mu_est
+            mu_ekf[:, i] = mu_est
 
-
-    plt.figure(1, figsize=(10,6))
-    ax1 = plt.subplot(121, aspect='equal')
-    ax1.plot(mu[0,:], mu[1,:], 'b')
-    ax1.plot(mu_ekf[0,:], mu_ekf[1,:], 'r--')
-    mf = m.reshape((-1,2))
-    ax1.scatter(mf[:,0], mf[:,1])
-    ax1.set_xlabel('X')
-    ax1.set_ylabel('Y')
+    plt.figure(1, figsize=(10, 6))
+    ax1 = plt.subplot(121, aspect="equal")
+    ax1.plot(mu[0, :], mu[1, :], "b")
+    ax1.plot(mu_ekf[0, :], mu_ekf[1, :], "r--")
+    mf = m.reshape((-1, 2))
+    ax1.scatter(mf[:, 0], mf[:, 1])
+    ax1.set_xlabel("X")
+    ax1.set_ylabel("Y")
 
     ax2 = plt.subplot(322)
-    ax2.plot(T, mu[0,:], 'b')
-    ax2.plot(T, mu_ekf[0,:], 'r--')
-    ax2.set_xlabel('t')
-    ax2.set_ylabel('X')
+    ax2.plot(T, mu[0, :], "b")
+    ax2.plot(T, mu_ekf[0, :], "r--")
+    ax2.set_xlabel("t")
+    ax2.set_ylabel("X")
 
     ax3 = plt.subplot(324)
-    ax3.plot(T, mu[1,:], 'b')
-    ax3.plot(T, mu_ekf[1,:], 'r--')
-    ax3.set_xlabel('t')
-    ax3.set_ylabel('Y')
+    ax3.plot(T, mu[1, :], "b")
+    ax3.plot(T, mu_ekf[1, :], "r--")
+    ax3.set_xlabel("t")
+    ax3.set_ylabel("Y")
 
     ax4 = plt.subplot(326)
-    ax4.plot(T, mu[2,:], 'b')
-    ax4.plot(T, mu_ekf[2,:], 'r--')
-    ax4.set_xlabel('t')
-    ax4.set_ylabel('psi')
+    ax4.plot(T, mu[2, :], "b")
+    ax4.plot(T, mu_ekf[2, :], "r--")
+    ax4.set_xlabel("t")
+    ax4.set_ylabel("psi")
 
     plt.figure(2)
     ax1 = plt.subplot(211)
--- Project 4/controllers/main/your_controller_ekf_slam.py
+++ Project 4/controllers/main/your_controller_ekf_slam.py
@@ -10,7 +10,6 @@ from ekf_slam import EKF_SLAM
 
 # CustomController class (inherits from BaseController)
 class CustomController(BaseController):
-
     def __init__(self, trajectory):
 
         super().__init__(trajectory)
@@ -23,11 +22,15 @@ class CustomController(BaseController):
         self.Iz = 25854
         self.m = 1888.6
         self.g = 9.81
-        
+
         self.counter = 0
         np.random.seed(99)
 
         # Add additional member variables according to your need here.
+        self.gains = np.matrix([20, 0.1, 5])
+        self.e_previous = 0
+        self.e_sum = 0
+        self.e_der = 0
 
     def getStates(self, timestep, use_slam=False):
 
@@ -36,38 +39,36 @@ class CustomController(BaseController):
         # Initialize the EKF SLAM estimation
         if self.counter == 0:
             # Load the map
-            minX, maxX, minY, maxY = -120., 450., -500., 50.
+            minX, maxX, minY, maxY = -120.0, 450.0, -500.0, 50.0
             map_x = np.linspace(minX, maxX, 7)
             map_y = np.linspace(minY, maxY, 7)
             map_X, map_Y = np.meshgrid(map_x, map_y)
-            map_X = map_X.reshape(-1,1)
-            map_Y = map_Y.reshape(-1,1)
+            map_X = map_X.reshape(-1, 1)
+            map_Y = map_Y.reshape(-1, 1)
             self.map = np.hstack((map_X, map_Y)).reshape((-1))
-            
+
             # Parameters for EKF SLAM
-            self.n = int(len(self.map)/2)             
+            self.n = int(len(self.map) / 2)
             X_est = X + 0.5
             Y_est = Y - 0.5
             psi_est = psi - 0.02
-            mu_est = np.zeros(3+2*self.n)
+            mu_est = np.zeros(3 + 2 * self.n)
             mu_est[0:3] = np.array([X_est, Y_est, psi_est])
             mu_est[3:] = np.array(self.map)
-            init_P = 1*np.eye(3+2*self.n)
-            W = np.zeros((3+2*self.n, 3+2*self.n))
-            W[0:3, 0:3] = delT**2 * 0.1 * np.eye(3)
-            V = 0.1*np.eye(2*self.n)
-            V[self.n:, self.n:] = 0.01*np.eye(self.n)
+            init_P = 1 * np.eye(3 + 2 * self.n)
+            W = np.zeros((3 + 2 * self.n, 3 + 2 * self.n))
+            W[0:3, 0:3] = delT ** 2 * 0.1 * np.eye(3)
+            V = 0.1 * np.eye(2 * self.n)
+            V[self.n :, self.n :] = 0.01 * np.eye(self.n)
             # V[self.n:] = 0.01
             print(V)
-            
+
             # Create a SLAM
             self.slam = EKF_SLAM(mu_est, init_P, delT, W, V, self.n)
             self.counter += 1
         else:
-            mu = np.zeros(3+2*self.n)
-            mu[0:3] = np.array([X, 
-                                Y, 
-                                psi])
+            mu = np.zeros(3 + 2 * self.n)
+            mu[0:3] = np.array([X, Y, psi])
             mu[3:] = self.map
             y = self._compute_measurements(X, Y, psi)
             mu_est, _ = self.slam.predict_and_correct(y, self.previous_u)
@@ -77,28 +78,28 @@ class CustomController(BaseController):
         print("True      X, Y, psi:", X, Y, psi)
         print("Estimated X, Y, psi:", mu_est[0], mu_est[1], mu_est[2])
         print("-------------------------------------------------------")
-        
+
         if use_slam == True:
             return delT, mu_est[0], mu_est[1], xdot, ydot, mu_est[2], psidot
         else:
             return delT, X, Y, xdot, ydot, psi, psidot
 
     def _compute_measurements(self, X, Y, psi):
-        x = np.zeros(3+2*self.n)
+        x = np.zeros(3 + 2 * self.n)
         x[0:3] = np.array([X, Y, psi])
         x[3:] = self.map
-        
+
         p = x[0:2]
         psi = x[2]
-        m = x[3:].reshape((-1,2))
+        m = x[3:].reshape((-1, 2))
 
-        y = np.zeros(2*self.n)
+        y = np.zeros(2 * self.n)
 
         for i in range(self.n):
             y[i] = np.linalg.norm(m[i, :] - p)
-            y[self.n+i] = wrapToPi(np.arctan2(m[i,1]-p[1], m[i,0]-p[0]) - psi)
-            
-        y = y + np.random.multivariate_normal(np.zeros(2*self.n), self.slam.V)
+            y[self.n + i] = wrapToPi(np.arctan2(m[i, 1] - p[1], m[i, 0] - p[0]) - psi)
+
+        y = y + np.random.multivariate_normal(np.zeros(2 * self.n), self.slam.V)
         # print(np.random.multivariate_normal(np.zeros(2*self.n), self.slam.V))
         return y
 
@@ -119,34 +120,62 @@ class CustomController(BaseController):
         _, true_X, true_Y, _, _, true_psi, _ = self.getStates(timestep, use_slam=False)
 
         # You are free to reuse or refine your code from P3 in the spaces below.
+        closest_distance, closest_idx = closestNode(X, Y, trajectory)
+
+        # * Lookahead point
+        lookahead = 150
+
+        lookahead_idx = closest_idx + lookahead
+        X_d = trajectory[min(lookahead_idx, len(trajectory) - 1)][0]
+        Y_d = trajectory[min(lookahead_idx, len(trajectory) - 1)][1]
+        psi_d = np.arctan2(Y_d - Y, X_d - X)
 
         # ---------------|Lateral Controller|-------------------------
-        """
-        Please design your lateral controller below.
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        """
+        # * State Space
+        A = np.zeros((4, 4))
+        A[0, 1] = 1
+        A[2, 3] = 1
+        A[1, 1] = -(4 * Ca) / (m * xdot)
+        A[1, 2] = (4 * Ca) / m
+        A[1, 3] = -(2 * Ca * (lf - lr)) / (m * xdot)
+        A[3, 1] = -(2 * Ca * (lf - lr)) / (Iz * xdot)
+        A[3, 2] = (2 * Ca * (lf - lr)) / Iz
+        A[3, 3] = -(2 * Ca * (lf ** 2 + lr ** 2)) / (Iz * xdot)
+        B = np.zeros((4, 1))
+        B[1, 0] = (2 * Ca) / m
+        B[3, 0] = (2 * Ca * lf) / Iz
+        C = np.zeros((1, 4))
+        C[0, 3] = 1
+        D = np.zeros((1, 1))
+
+        discrete_system = signal.cont2discrete((A, B, C, D), delT, method="zoh")
+        Ad = discrete_system[0]
+        Bd = discrete_system[1]
+
+        # * LQR Controller
+        Q = np.array([[10, 0, 0, 0], [0, 1, 0, 0], [0, 0, 100, 0], [0, 0, 0, 10]])
+        R = np.array([1])
+
+        S = linalg.solve_discrete_are(Ad, Bd, Q, R)
+        K = -linalg.inv(R + B.T @ S @ B) @ B.T @ S @ A
+
+        e1 = (Y - Y_d) * np.cos(psi_d) + (X - X_d) * np.sin(psi_d)
+        e2 = wrapToPi(psi - psi_d)
+        e1dot = ydot * np.cos(e2) + xdot * np.sin(e2)
+        e2dot = psidot
+        lateral_errors = [[e1], [e1dot], [e2], [e2dot]]
+
+        delta = float(K @ lateral_errors)
 
         # ---------------|Longitudinal Controller|-------------------------
-        """
-        Please design your longitudinal controller below.
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        """
+        # * PID Controller
+        e = np.sqrt((X_d - X) ** 2 + (Y_d - Y) ** 2)
+        self.e_sum += e
+        self.e_der = e - self.e_previous
+        self.e_previous = e
+        longitudinal_errors = np.matrix([[e], [self.e_sum * delT], [self.e_der / delT]])
+
+        F = float(self.gains @ longitudinal_errors)
 
         # Return all states and calculated control inputs (F, delta)
         return true_X, true_Y, xdot, ydot, true_psi, psidot, F, delta
